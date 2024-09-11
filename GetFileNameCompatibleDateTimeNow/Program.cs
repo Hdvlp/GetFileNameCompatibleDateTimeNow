398857ba55581280436db0538f2be04c09106ec3
Hdvlp <hungtin.hongkong@yahoo.com.hk>
2024-09-11 11:34:52 +0800

    Add project files.

---
+++ GetFileNameCompatibleDateTimeNow/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Why?
+// To reliably get the YearMonthDate_HourMinuteSecond_TimeZone string
+
+using System.Linq;
+using System.IO;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine(DateTimeTools.GenerateDateTime());
+
+        }
+    }
+}
